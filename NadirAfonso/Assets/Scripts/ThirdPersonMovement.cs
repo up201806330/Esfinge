317a2815b2a6534a3015e5b3c09717272e9cb8fb
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-11-14 18:44:39 +0000

    Movement animations

--- NadirAfonso/Assets/Scripts/ThirdPersonMovement.cs
+++ NadirAfonso/Assets/Scripts/ThirdPersonMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,20 +7,34 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Animator animator;
 
-    public float speed = 6f;
+    public float speed = 3f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     public float gravity = 1f;
+    public float runSpeed = 2f;
+
+    int wlkHash;
+    int rngHash;
+
+    private void Start() {
+        animator = GetComponentInChildren<Animator>();
+
+        wlkHash = Animator.StringToHash("Walking");
+        rngHash = Animator.StringToHash("Running");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        bool shift = Input.GetKey(KeyCode.LeftShift);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        animationStateMachine(direction.magnitude >= 0.1f, shift);
 
         if (direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -27,9 +42,20 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * Time.deltaTime * (shift ? runSpeed: 1));
 
             controller.Move(new Vector3(0, -gravity, 0));
         }
+    }
+
+    private void animationStateMachine(bool walkingNow, bool runningNow) {
+        bool isWalking = animator.GetBool(wlkHash);
+        bool isRunning = animator.GetBool(rngHash);
+
+        if (!isWalking && walkingNow) animator.SetBool(wlkHash, true);
+        if (isWalking && !walkingNow) animator.SetBool(wlkHash, false);
+
+        if (!isRunning && (walkingNow && runningNow)) animator.SetBool(rngHash, true);
+        if (isRunning && (!walkingNow || !runningNow)) animator.SetBool(rngHash, false);
     }
 }
